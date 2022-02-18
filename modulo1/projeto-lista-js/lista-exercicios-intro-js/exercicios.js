2ad07bcac44a3ad59d639b0763410379a6cadbf1
danivaz2022 <daniornela@gmail.com>
2022-02-18 20:55:54 -0300

    projeto

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -19,43 +19,58 @@ function imprimeMensagem() {
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
   // implemente sua lógica aqui
-
+let larguraRetangulo = Number (prompt("digite a largura do retangulo : "))
+let alturaRetangulo = Number (prompt("digite a altura do retangulo : "))
+let resultadoDaArea = larguraRetangulo * alturaRetangulo
+console.log(resultadoDaArea)
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
-
+let anoHoje = Number (prompt("digite o ano que estamos atualmente: "))
+let anoNascimento = Number(prompt("digite o ano do seu nascimento: "))
+let idade = anoHoje - anoNascimento
+console.log (idade)
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
-
+return peso / ( altura * altura)
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+const nome = (prompt("Qual seu nome?"))
+const idade = Number(prompt("Qual sua idade?"))
+const email = (prompt("digite seu e-mail:"))
+console.log (`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
-
+const coresFavoritas = []
+const corFavorita1 = prompt("")
+const corFavorita2 = prompt("")
+const corFavorita3 = prompt("")
+const coresAdicionadas = coresFavoritas.push(corFavorita1, corFavorita2, corFavorita3)
+console.log(coresFavoritas)
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-
+let fraseMinuscula = "viva a vida"
+console.log(fraseMinsuscula.toUppercase())
 }
 
 // EXERCÍCIO 07
-function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
+function calculaIngressosEspetaculo(custo, valorIngressos) {
+  // implemente sua lógica 
 
 }
 
@@ -64,7 +79,6 @@ function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
 
 }
-
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
