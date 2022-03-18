d2366b61f392e957b8456908735d85938ac9ca73
danivaz2022 <daniornela@gmail.com>
2022-03-18 19:42:54 -0300

    revisao-js

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,8 @@
+//- **xercícios de fixação do conhecimento**    
+//Faça um resumo sobre quais são os 7 passos fundamentais para a resolução de problemas e como eles podem nos ajudar a solucionar nossos desafios de programação.
+//1 Ler o enunciado com atenção identificando palavras novas, para buscar conhecimento do significado de cada termo para garantiro desenvolvimento do projeto;
+//2 Retirar as palavras chaves do enunciado tanto nas variaveis , funções e parametros e buscar o que se pode transformar em valor n o código;
+//3 Criar expectativas de entradas e saidas que estes  codigos iram gerar;
+//4 Fazer um esquema visual como fluxograma ou tópicos de texto;
+//5 Criar um código que utilize os dados do esquema ou do fluxograma, pois neste momento esse código ainda não está preoucupado com performace;
+//6 Testar o código e se os testes indicarem o código roda devemos tratar de deixa-lo mais legivel.
