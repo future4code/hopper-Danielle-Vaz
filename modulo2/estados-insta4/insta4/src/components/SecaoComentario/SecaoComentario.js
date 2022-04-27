c017f474e28352ae04f6d188fc7ae6e57de1fec7
danivaz2022 <daniornela@gmail.com>
2022-04-26 21:27:42 -0300

    estados-insta4

---
+++ modulo2/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComentario = styled.input`
+    width: 100%;
+    margin-right: 5px;
+`
+
+export class SecaoComentario extends Component {
+	state = {
+resposta:""
+	  }
+	
+	  onChangeComentario = (e) => {
+		this.setState(
+		  {
+			resposta:e.target.value,
+		  }
+		)
+		
+	  }
+	render() {
+		return <CommentContainer>
+			<InputComentario
+				placeholder={'Comentário'}
+				value={this.state.resposta}
+				onChange={this.onChangeComentario}
+			/>
+			<button onClick={this.props.aoEnviar}>Enviar</button>
+		</CommentContainer>
+	}
+}
